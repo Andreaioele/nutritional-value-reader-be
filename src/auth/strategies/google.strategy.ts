ab50a74e24d0b88400c185b2e660432de5dfdd7d
Andrea Ioele <a.ioele@icloud.com>
2024-07-23 12:08:54 +0200

    [auth] - move secret in env

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -2,13 +2,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import * as process from "node:process";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private readonly authService: AuthService) {
     super({
-      clientID: '147700121593-null6hkqg4849csorsb7lcbhpq53eato.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientID: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_SECRET,
       callbackURL: 'http://localhost:3011/auth/google/callback',
       scope: ['email', 'profile'],
     });
